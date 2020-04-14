10734fa0ad262ad61d5733aa703c897b7a663519
Paco <paco@Pacos-MacBook-Pro.local>
2020-04-13 20:28:57 -0400

    update membership page

--- src/app/membership/membership.component.ts
+++ src/app/membership/membership.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AuthService } from '../service/auth.service';
+import { ProfileService } from '../service/profile.service';
 
 @Component({
   selector: 'app-membership',
@@ -9,14 +10,33 @@ import { AuthService } from '../service/auth.service';
 export class MembershipComponent implements OnInit {
   public mobileWidth;
   public mobileMarg;
+  public active;
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private profileService: ProfileService) {
+  }
 
   ngOnInit() {
     if (this.auth.isMobile()) {
       this.mobileWidth = { "width": "900px", };
       this.mobileMarg = { "margin": "20px" };
     }
+
+
+    if (this.auth.isLoggedIn()) {
+      this.hasProfile();
+    }
+  }
+
+  hasProfile() {
+    this.profileService.getProfile(this.auth.getUser().email).subscribe(data => {
+      if (data[0] !== undefined) {
+        console.log(data);
+        this.active = true;
+      } else {
+        this.active = false;
+      }
+      console.log(this.active);
+    });
   }
 
 }
