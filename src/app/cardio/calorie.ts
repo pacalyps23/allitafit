82c162e3a13a1d2d140ad9535ca496676f1c57b8
Paco <paco@Pacos-MacBook-Pro.local>
2020-05-09 10:28:59 -0400

    fixed duplicate error on calorie admin

--- src/app/cardio/calorie.ts
+++ src/app/cardio/calorie.ts
@@ -1,12 +1,12 @@
 export class Calorie {
     name: string;
-    week1: Array<number>;
-    week2: Array<number>;
-    week3: Array<number>;
-    week4: Array<number>;
+    week1: Array<Number>;
+    week2: Array<Number>;
+    week3: Array<Number>;
+    week4: Array<Number>;
 
-    constructor(name:string, week1: Array<number>, week2:Array<number>, week3:Array<number>, 
-        week4:Array<number>, week1Tot:number, week2Tot: number, week3Tot:number, week4Tot:number){
+    constructor(name:string, week1: Array<Number>, week2:Array<Number>, week3:Array<Number>, 
+        week4:Array<Number>, week1Tot:number, week2Tot: number, week3Tot:number, week4Tot:number){
         this.name = name;
         this.week1 = week1;
         this.week2 = week2;
